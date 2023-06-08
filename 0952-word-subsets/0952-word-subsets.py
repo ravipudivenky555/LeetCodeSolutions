bcd56e3ea0c4a0e56cc92d8db34482e232734a32
ravipudivenky555 <66905192+ravipudivenky555@users.noreply.github.com>
2023-06-08 20:32:24 +0530

    Time: 1120 ms (31.7%), Space: 20.6 MB (70.09%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.py
@@ -0,0 +1,10 @@
+class Solution:
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        result = []
+        tempDict = Counter()
+        for w in words2:
+            tempDict |= Counter(w)
+        for w in words1:
+            if not tempDict - Counter(w):
+                result.append(w)
+        return result
